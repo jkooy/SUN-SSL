8dfc14c877a06013b3373b87cf3834835a100097
Sheehan <x5he@ucsd.edu>
2020-10-30 21:06:58 -0700

    Update preprocessing.bash

--- preprocessing.bash
+++ preprocessing.bash
@@ -1,3 +1,6 @@
+wget http://www.image-net.org/challenges/LSVRC/2010/d5ef8751a0a1077596a929e9a224ee01/non-pub/ILSVRC2010_images_train.tar
+wget http://www.image-net.org/challenges/LSVRC/2010/d5ef8751a0a1077596a929e9a224ee01/non-pub/ILSVRC2010_images_val.tar
+
 
 dir=./
  
