eb9f5dff3d0076e1599c2762724fa66be009ec77
Sheehan <x5he@ucsd.edu>
2020-11-06 04:18:51 -0800

    Update preprocessing.bash

--- preprocessing.bash
+++ preprocessing.bash
@@ -1,17 +1,23 @@
 wget http://www.image-net.org/challenges/LSVRC/2010/d5ef8751a0a1077596a929e9a224ee01/non-pub/ILSVRC2010_images_train.tar
 wget http://www.image-net.org/challenges/LSVRC/2010/d5ef8751a0a1077596a929e9a224ee01/non-pub/ILSVRC2010_images_val.tar
 
+tar -xvf ILSVRC2012_img_train.tar && rm -f ILSVRC2012_img_train.tar 
+find . -name "*.tar" | while read NAE ; do mkdir -p "${NAE%.tar}"; tar -xvf "${NAE}" -C "${NAE%.tar}"; rm -f "${NAE}"; done
 
-dir=./
+bash ../../imagenet_swav10.sh
+
+
+
+# dir=./
  
-for x in `ls *.tar`
+# for x in `ls *.tar`
  
-do
+# do
  
-    filename=`basename $x .tar`
+#     filename=`basename $x .tar`
  
-    mkdir $filename
+#     mkdir $filename
  
-    tar -xvf $x -C ./$filename
+#     tar -xvf $x -C ./$filename
  
-done
+# done
